14079fff2ac9170c9c4a0978c0d92a1cb7676012
DiegoZamora33 <diego.zamora33.dz@gmail.com>
2021-05-19 18:03:26 -0500

    Guardar Contactos

---
+++ iContacts/iContacts/Contacto.swift
@@ -0,0 +1,14 @@
+//
+//  Contacto.swift
+//  iContacts
+//
+//  Created by Diego Zamora on 19/05/21.
+//
+
+import Foundation
+
+struct Contacto {
+    let nombre: String?
+    let telefono: Int64?
+    let direccion: String?
+}
--- iContacts/iContacts/ViewController.swift
+++ iContacts/iContacts/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     // MARK: - Outlets y Variables
     @IBOutlet weak var miTabla: UITableView!
+    var misContactos = [Contacto]()
     
 
     override func viewDidLoad() {
@@ -23,8 +24,50 @@ class ViewController: UIViewController {
         /// REgistrar mi Celda Personalizada
         miTabla.register(UINib(nibName: "ContactoTableViewCell", bundle: nil), forCellReuseIdentifier: "miCeldaCustom")
     }
-
-
+    
+    // MARK: - Agregar nuevo Contacto
+    @IBAction func addContacto(_ sender: UIBarButtonItem) {
+        
+        /// Creamos mi Alerta
+        let alert = UIAlertController(title: "Nuevo Contacto", message: "Llena los siguientes campos", preferredStyle: .alert)
+        
+        /// Creamos mis acciones
+        let actionOK = UIAlertAction(title: "Guardar", style: .default) {
+            (_) in
+            
+            /// Get Nombre
+            guard let nombre = alert.textFields?.first?.text else {return}
+            
+            /// Get Telefono
+            guard let telefono = Int64((alert.textFields?.last?.text ?? nil)!) else {return}
+            
+            /// Apend Contacto Nuevo
+            self.misContactos.append(Contacto(nombre: nombre, telefono: telefono, direccion: "HERE"))
+            
+            /// Reload Data
+            self.miTabla.reloadData()
+        }
+        
+        let actionCancel = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        /// Agregamos acciones a la Alerta
+        alert.addAction(actionOK)
+        alert.addAction(actionCancel)
+        
+        /// Agregamos los TextField
+        alert.addTextField { (nombre: UITextField) in
+            nombre.placeholder = "Nombre"
+        }
+        
+        alert.addTextField { (nombre: UITextField) in
+            nombre.placeholder = "Telefono"
+        }
+        
+        
+        /// Presentamos mi Alerta
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
 // MARK: - Extension para UITableView
@@ -32,7 +75,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     /// Numero de Rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return misContactos.count
     }
     
     /// Render de cada Row
@@ -41,11 +84,15 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         /// Mi celda Reutilizable
         let celda = miTabla.dequeueReusableCell(withIdentifier: "miCeldaCustom", for: indexPath) as! ContactoTableViewCell
         
-        celda.nombre.text = "Nombre Contacto"
-        celda.numero.text = "Telefono"
+        celda.nombre.text = self.misContactos[indexPath.row].nombre
+        celda.numero.text = " 📞 \(String(describing: self.misContactos[indexPath.row].telefono!))"
         
         return celda
     }
     
-    
+    /// Height por cada Row
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 78
+    }
 }
